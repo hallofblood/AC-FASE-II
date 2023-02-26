cc50bee8acd167050ce13dd4c016413fc5603edb
Victor Padron Garcia <vpg66@alu.ua.es>
2023-02-26 11:51:36 +0100

    Falta calcular el tiempo que tarda en realizar la operacion

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
-
+#include <time.h>
 using namespace std;
 int main() {
-	int iteracionActual = 0;
-	int siguiente = 1;
-	int iteraciones;
+	char opcion;
+	do {
+		long iteracionActual = 0;
+		long siguiente = 1;
+		long aux;
 
-	cout << "Introduce cuantas iteraciones de la serie de fibonacci quieres realizar: ";
-	cin >> iteraciones;
+		long iteraciones;
 
-	while (iteraciones > 0) {
-		int aux = siguiente;
-		siguiente = iteracionActual + siguiente;
-		iteracionActual = aux;
-		iteraciones--;
-	}
-	cout << iteracionActual;
+		bool valido = false;
+		while (valido == false) {
+			cout << "Introduce cuantas iteraciones de la serie de fibonacci quieres realizar: ";
+			cin >> iteraciones;
+			if (iteraciones <= 0) {
+				cout << "Numero de iteraciones no valido" << endl;
+			}
+			else {
+				valido = true;
+			}
+		}
+		auto tiempoInicial = clock();
+		while (iteraciones > 0) {
+			aux = siguiente;
+			siguiente = iteracionActual + siguiente;
+			iteracionActual = aux;
+			iteraciones--;
+		}
+		auto tiempoFinal = clock();
+		cout << iteracionActual <<endl;
+		cout << tiempoFinal  << endl;
+
+		cout << "Quiere continuar? (s/n): ";
+		cin >> opcion;
+	} while (opcion != 'n');
 	return 0;
 }
