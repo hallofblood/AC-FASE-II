b35339d7e9cf684873062fa2574f6209eb3af710
agmg16-ua <agmg16@alu.ua.es>
2023-02-27 10:19:19 +0100

    Started x86 phase

---
+++ Fibonacci/Fibonacci_x86.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main() {
+	int n;
+	int itAct = 0;
+	int sigNum = 1;
+
+	cout << "Introduce cuantas iteraciones de la serie de Fibonacci quieres hacer: ";
+	cin >> n;
+
+	__asm {
+
+	}
+}
