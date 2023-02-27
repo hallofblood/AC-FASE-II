9d1a8c3fa1e9a3b5de2a83449132701aa9daea23
Victor Padron Garcia <vpg66@alu.ua.es>
2023-02-27 10:58:57 +0100

    "Esta hecho todo, pero como tarda tan poco el algoritmo el tiempo que tarda es igual a 0, esto cambia a partir de 100000 iteraciones pero esa cantidad no la aguanta la variable que almacena el valor"

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -4,13 +4,15 @@ using namespace std;
 int main() {
 	char opcion;
 	do {
-		long iteracionActual = 0;
-		long siguiente = 1;
-		long aux;
-
-		long iteraciones;
-
-		bool valido = false;
+		long long iteracionActual = 0;		//Almacena el valor en la primera iteracion con el valor anterior
+		long long siguiente = 1;				//Almacena el valor de la siguiente operacion de sumar los valores n + (n-1)
+		long long aux;					//Auxiliar para intercambiar los valores de iteración actual y siguiente
+		long long iteraciones;		//Iteraciones a realizar
+		clock_t tiempoInicial = 0;	//Almaceno el tiempo inicial antes de ejecutar el algoritmo
+		clock_t tiempoFinal = 0;		//Almacenan el tiempo final de ejecutar mi algoritmo
+		
+		//Compruebo si el numero de iteraciones es valido
+		bool valido = false;	
 		while (valido == false) {
 			cout << "Introduce cuantas iteraciones de la serie de fibonacci quieres realizar: ";
 			cin >> iteraciones;
@@ -21,17 +23,19 @@ int main() {
 				valido = true;
 			}
 		}
-		auto tiempoInicial = clock();
+
+		//Bucle que realiza la serie de fibonacci
+		tiempoInicial = clock();
 		while (iteraciones > 0) {
 			aux = siguiente;
 			siguiente = iteracionActual + siguiente;
 			iteracionActual = aux;
 			iteraciones--;
 		}
-		auto tiempoFinal = clock();
-		cout << iteracionActual <<endl;
-		cout << tiempoFinal  << endl;
+		tiempoFinal = clock();
 
+		cout << iteracionActual <<endl;
+		cout << 1000.0 * (tiempoFinal - tiempoInicial)/CLOCKS_PER_SEC << endl;
 		cout << "Quiere continuar? (s/n): ";
 		cin >> opcion;
 	} while (opcion != 'n');
