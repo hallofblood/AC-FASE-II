9038ee2ea82585c6ed33999bf0240ae41f08306a
agmg16-ua <agmg16@alu.ua.es>
2023-03-03 13:44:53 +0100

    Main terminado a falta de los algoritmos

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,20 +1,65 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+
 
 using namespace std;
+
+void fibonacciC() {
+
+}
+
+void fibonaccix86() {
+
+}
+
+void fibonacciSSE() {
+
+}
+
+
 int main() {
-	int iteracionActual = 0;
-	int siguiente = 1;
-	int iteraciones;
+	char seguir;
+	do {
 
-	cout << "Introduce cuantas iteraciones de la serie de fibonacci quieres realizar: ";
-	cin >> iteraciones;
+		int opc;
+		cout << "Bienvenido a la calculadore de Fibonacci. \n"
+			<< "1. Fibonacci en C++ \n"
+			<< "2. Fibonacci en x86 \n"
+			<< "3. Fibonacci en SSE \n"
+			<< "Seleccione una opcion: ";
+		do {
+			cin >> opc;
 
-	while (iteraciones > 0) {
-		int aux = siguiente;
-		siguiente = iteracionActual + siguiente;
-		iteracionActual = aux;
-		iteraciones--;
-	}
-	cout << iteracionActual;
+			if (opc < 1 || opc > 3) {
+				cout << "Opcion no valida, vuelve a introducirla: ";
+			}
+
+		} while (opc < 1 || opc > 3);
+
+		switch (opc) {
+		case 1: fibonacciC();
+			break;
+
+		case 2: fibonaccix86();
+			break;
+
+		case 3: fibonacciSSE();
+		}
+
+		cout << "Indique si quiere probar otra forma(s/n): ";
+
+		do {
+			cin >> seguir;
+
+			if (seguir != 's' && seguir != 'n') {
+				cout << "Opcion no valida, vuelve a introducirla: ";
+			}
+
+		} while (seguir != 's' && seguir != 'n');
+
+	} while (seguir != 'n');
+
 	return 0;
 }
